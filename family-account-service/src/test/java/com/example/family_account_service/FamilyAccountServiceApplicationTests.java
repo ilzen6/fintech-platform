aa99a414246c77d03dafe404e94644b89742a262
ilzen6 <zenckovitch.ilya@yandex.ru>
2026-03-24 07:35:25 +0300

    chore(project): initial project structure with all services and docker-compose

---
+++ family-account-service/src/test/java/com/example/family_account_service/FamilyAccountServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.family_account_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilyAccountServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
